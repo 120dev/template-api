6eeedd17eb8eecf53bc066b77822514234c70bd6
Candir vincent <120@dev.nc>
2017-05-08 15:32:52 +1100

    fresh L5.4, DingoApi, PHPunit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->resource('posts', 'App\DEV\Controllers\PostsController');
+    $api->resource('categories', 'App\DEV\Controllers\CategoriesController');
+});
